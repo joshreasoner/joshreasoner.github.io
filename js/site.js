997e70db68de4ee6b35473fc6e86c9bc2375b8dd
joshreasoner <107153028+joshreasoner@users.noreply.github.com>
2025-03-09 00:42:25 -0800

    new headshot

--- js/site.js
+++ js/site.js
@@ -1,8 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-//TypeWrite Effect
+﻿//TypeWrite Effect
 let myText = document.querySelector('#subtitle');
 const words = ["Army veteran", "Software developer", "Father", "Humanitarian"];
 let i = 0;
